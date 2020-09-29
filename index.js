3be7c9a2206a4bd18dfaf0db4bd3529a48ca216e
bonasunu <bonaventurasunu@gmail.com>
2020-09-30 01:17:48 +0700

    add logger for info

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http')
 const app = require('./app')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 const port = config.PORT || 5000
 
-server.listen(port, () => console.log(`App listen on port ${port}`))
+server.listen(port, () => logger.info(`App listen on port ${port}`))
